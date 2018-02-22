98cb19824975efaeeb5e40a3130bbdc68391aa2e
CruzMayra <velazquez.leonora@gmail.com>
2018-02-21 20:16:24 -0600

    Solución del ejercicio número dos

---
+++ 2.-squareOfPairs/js/app.test.js
@@ -0,0 +1,11 @@
+describe("square", () => {
+  it("returns the elements squared only if the number is positive if passed an array of numbers", () => {
+    expect(square([2, 4, 6])).toEqual([4, 16, 36]);
+  });
+  it("returns the elements squared only if the number is positive if passed an array of numbers", () => {
+    expect(square([-3, 2, -8, 12, 5])).toEqual([4, 144, 25]);
+  });
+  it("returns the elements squared only if the number is positive if passed an array of numbers", () => {
+    expect(square([1, 2, 3, 4, 5])).toEqual([1, 4, 9, 16, 25]);
+  });
+});
