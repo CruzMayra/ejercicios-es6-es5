73b0a7465e0dfdb42bf38ca1dc374a5852a7c963
CruzMayra <velazquez.leonora@gmail.com>
2018-02-22 00:49:13 -0600

    Corriguiendo redacción de pruebas

--- 2.-squareOfPairs/js/app.test.js
+++ 2.-squareOfPairs/js/app.test.js
@@ -2,7 +2,7 @@ describe("square", () => {
   it("returns the elements squared only if the number is positive if passed an array of numbers", () => {
     expect(square([2, 4, 6])).toEqual([4, 16, 36]);
   });
-  it("returns the elements squared only if the number is positive if passed an array of numbers", () => {
+  it("returns the elements squared only if the number is positive if passed an array of negative numbers", () => {
     expect(square([-3, 2, -8, 12, 5])).toEqual([4, 144, 25]);
   });
   it("returns the elements squared only if the number is positive if passed an array of numbers", () => {
